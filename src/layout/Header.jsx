660f4da9c16a0ded950edb66b1de90ebd91d9a32
LKJ970524 <dlrbwjd7096@naver.com>
2023-09-07 14:12:06 +0900

    [Modify] 리액트 테스트 - 테스트 코드를 작성하였습니다.

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+import Nav from "./nav"
+
+function Header() {
+  return (
+    <header className="text-9xl">
+      여기가 헤더부분
+      <Nav/>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/layout/Nav.jsx
@@ -0,0 +1,7 @@
+function Nav() {
+  return (
+    <div className="text-2xl">navigation</div>
+  )
+}
+
+export default Nav
