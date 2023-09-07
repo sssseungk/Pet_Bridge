b7af90de58553825ffb963b4082506e3a4498719
sssseungk <cacy0118@naver.com>
2023-09-08 03:11:54 +0900

    :hammer: 헤더 컴포넌트, 상품목록 페이지 내비게이션 컴포넌트 생성

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,10 +1,24 @@
 import Nav from "./nav"
+import HeaderTitle from './../components/HeaderTitle';
+import ProductList from './ProductList';
+
+
+// title='펫:브릿지', showLogo=false, showSearchIcon=false, showCartIcon=false
 
 function Header() {
+
   return (
-    <header className="text-9xl">
-      여기가 헤더부분
+    <header>
+      {/*
+        <HeaderTitle title="펫:브릿지" showLogo={true} showSearchIcon={true} showCartIcon={true}/>
+        <HeaderTitle title="장바구니"/>
+        <HeaderTitle title="유기동물 봉사활동"/>
+        <HeaderTitle title="내 주변 보호소 찾기"/>
+        <HeaderTitle title="상품상세정보" showCartIcon={true}/>
+      */}
+      <HeaderTitle title="후원 상품" showSearchIcon={true} showCartIcon={true}/>
       <Nav/>
+      <ProductList/>
     </header>
   )
 }
--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -1,6 +1,12 @@
 function Nav() {
   return (
-    <div className="text-2xl">navigation</div>
+    <ul className="bg-pet-bg font-bold flex gap-4 px-8 justify-between flex-wrap pt-6 border-gray-1 relative">
+      <li className="border-pet-orange border-b-2 z-10 text-pet-orange py-2 xs:w-auto">베스트</li>
+      <li className="py-2 xs:w-auto">신상품</li>
+      <li className="py-2 xs:w-auto">전체</li>
+      <li className="py-2 xs:w-auto">무료배송</li>
+      <div className="absolute bottom-0 left-0 w-full border-gray-1 border-b-2"></div>
+    </ul>
   )
 }
 
