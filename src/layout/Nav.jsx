8cd070008b26a25efd1a8087f2f94bcea3087cea
LKJ970524 <dlrbwjd7096@naver.com>
2023-09-08 17:31:23 +0900

    [Modify] nav 마크업
    - nav 마크업 수정했습니다
    
    #10 작업중

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -7,16 +7,15 @@ import myPage from '@/assets/icons/mypage2.svg';
 function Nav() {
   return (
     <>
-    <nav className="w-full max-w-4xl h-[52px] bg-pet-bg px-[28px] py-[16px] bottom-0 fixed rounded-b-xl left-0 right-0 m-auto">
-      <div className='flex justify-between items-center'>
-              <button type="submit" className=''><img src={home} alt='메인' /></button>
-      <button type="submit"><img src={shop} alt="쇼핑" /></button>
-      <button type="submit"><img src={map} alt="맵" /></button>
-      <button type="submit"><img src={myPage} alt="마이페이지" /></button>
-      </div>
-
-    </nav>
-</>
+      <nav className="w-full max-w-4xl h-[52px] bg-pet-bg px-[28px] py-[16px] bottom-0 fixed rounded-b-xl left-0 right-0 m-auto border">
+        <div className='flex justify-between items-center'>
+          <button type="submit" className=''><img src={home} alt='메인' /></button>
+          <button type="submit"><img src={shop} alt="쇼핑" /></button>
+          <button type="submit"><img src={map} alt="맵" /></button>
+          <button type="submit"><img src={myPage} alt="마이페이지" /></button>
+        </div>
+      </nav>
+    </>
 
     
   )
