236f509de3089e15b9d95559d9649d77ab14f67d
sssseungk <cacy0118@naver.com>
2023-09-08 16:50:44 +0900

    [Add] HeaderComp 컴포넌트 props 사용하여 모든 페이지 헤더 생성
    #18 진행중

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,15 @@
+import HeaderComp from "@/components/Header/HeaderComp"
+import Nav from './Nav';
+
 function Header() {
   return (
-    <header className="text-2xl text-pet-red">
-      Header
+    <header>
+      <HeaderComp title="펫:브릿지" showLogo={true} showSearchIcon={true} showCartIcon={true}/>
+      <HeaderComp title="장바구니"/>
+      <HeaderComp title="유기동물 봉사활동"/>
+      <HeaderComp title="내 주변 보호소 찾기"/>
+      <HeaderComp title="상품상세정보" showCartIcon={true}/>
+      <HeaderComp title="후원 상품" showSearchIcon={true} showCartIcon={true}/>
     </header>
   )
 }
