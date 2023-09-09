85a2cd07bdc26480a9b2bb08562d4a98f9924662
sssseungk <cacy0118@naver.com>
2023-09-09 21:01:52 +0900

    [Modify] 헤더 아이콘 기능 추가 - prev 아이콘 클릭 시 이전 페이지로 이동 - 장바구니 아이콘 클릭 시 장바구니 페이지로 이동 - 검색 버튼 클릭 시 검색 페이지로 이동

--- src/components/Header/HeaderComp.jsx
+++ src/components/Header/HeaderComp.jsx
@@ -3,13 +3,21 @@ import logo_header_icon from '/assets/icons/logo_header_icon.svg'
 import search_icon from '/assets/icons/search_icon.svg'
 import cart_off_icon from '/assets/icons/cart_off_icon.svg'
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function HeaderComp({title, showLogo, showSearchIcon, showCartIcon}) {
+  
+  const navigate = useNavigate();
+  const handleGoPrevPage = () => {
+    navigate(-1);
+  }
+
   return (
     <>
       <div className="max-w-4xl mx-auto p-5 bg-pet-bg border-gray-800 shadow-lg relative z-10">
         <div className="flex items-center justify-between">
-          <button>
+        <button onClick={handleGoPrevPage}>
             <img src={prev_icon}/>
           </button>
           <h1 className="flex items-center gap-1 font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -18,10 +26,14 @@ function HeaderComp({title, showLogo, showSearchIcon, showCartIcon}) {
           </h1>
           <div>
             <button className="pr-4">
-              {showSearchIcon && <img src={search_icon}/>}
+              <Link to='search'>
+                {showSearchIcon && <img src={search_icon}/>}
+              </Link>
             </button>
             <button>
-              {showCartIcon && <img src={cart_off_icon}/>}
+              <Link to='cart'>
+                {showCartIcon && <img src={cart_off_icon}/>}
+              </Link>
             </button>
           </div>
         </div>
