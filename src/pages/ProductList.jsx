28736bf18d659507e573297a642bf3d784be1322
sssseungk <cacy0118@naver.com>
2023-09-09 14:33:42 +0900

    [Add] 상품 아이템 컴포넌트 포함하는 상품 목록 페이지 생성(ProductList)
    #23 진행중

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,6 +1,22 @@
+import HeaderComp from "@/components/Header/HeaderComp"
+import ProductItem from "@/components/ProductList/ProductItem"
+import ProductListNav from './../components/ProductList/ProductListNav';
+
 function ProductList() {
+  
   return (
-    <div>ProductList</div>
+    <div>
+      <HeaderComp title="후원 상품" showSearchIcon={true} showCartIcon={true}/>
+      <ProductListNav/>
+      <ol className="flex flex-wrap max-w-4xl mx-auto justify-center mt-5 gap-2">
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+        <ProductItem/>
+      </ol>
+    </div>
   )
 }
 
