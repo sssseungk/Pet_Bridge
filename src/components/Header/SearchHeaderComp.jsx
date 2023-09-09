187cdc7614085d54bcd6072e2df5e04592f4a60f
sssseungk <cacy0118@naver.com>
2023-09-09 15:12:42 +0900

    [Add] 제품 검색 페이지 헤더 컴포넌트 생성
    #29 진행중

---
+++ src/components/Header/SearchHeaderComp.jsx
@@ -0,0 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import prev_icon from '@/assets/icons/prev_icon.svg'
+import { useState } from "react";
+
+function SearchHeaderComp() {
+
+  const navigate = useNavigate();
+  const handleGoPrevPage = () => {
+    navigate(-1);
+  }
+
+  const [search, setSearch] = useState("");
+  const onChange = (e) => {
+      setSearch(e.target.value)
+  }
+
+  return (
+    <>
+      <div className="max-w-4xl mx-auto p-5 bg-pet-bg border-gray-800 shadow-lg relative z-10">
+        <div className="flex items-center justify-between">
+          <button className="flex items-center">
+            <img src={prev_icon} alt="Previous" className="mr-2" onClick={handleGoPrevPage}/>
+          </button>
+          <form action='/' className="ml-4 w-full">
+            <fieldset>
+              <legend className="sr-only">검색 폼</legend>
+              <label className="sr-only" htmlFor="search">검색</label>
+              <input
+                type="search"
+                id="search"
+                value={search}
+                onChange={onChange}
+                placeholder="상품명을 입력하세요."
+                className="w-full bg-[#E9E9E9] text-[#9DA1B4] py-1 px-2 rounded-lg focus:outline-none"
+              />
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+export default SearchHeaderComp
