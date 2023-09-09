18359d2c3200a7ac6ef545951d6b6251bd99743d
sssseungk <cacy0118@naver.com>
2023-09-09 23:00:45 +0900

    [Modify] 상품 목록 페이지 헤더 컴포넌트 제거
    #23 진행중

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,4 +1,3 @@
-import HeaderComp from "@/components/Header/HeaderComp"
 import ProductItem from "@/components/ProductList/ProductItem"
 import ProductListNav from './../components/ProductList/ProductListNav';
 
@@ -6,7 +5,6 @@ function ProductList() {
   
   return (
     <div>
-      <HeaderComp title="후원 상품" showSearchIcon={true} showCartIcon={true}/>
       <ProductListNav/>
       <ol className="flex flex-wrap max-w-4xl mx-auto justify-center mt-5 gap-2">
         <ProductItem/>
