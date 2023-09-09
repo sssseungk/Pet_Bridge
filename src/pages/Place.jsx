e738eacd85fa95c6b501eb9c646363011dcea402
tuna7797 <gmlwjd7731@gmail.com>
2023-09-09 16:33:12 +0900

    [Modify] place 마크업 - place 레이아웃 구조 작성 #25

--- src/pages/Place.jsx
+++ src/pages/Place.jsx
@@ -1,6 +1,49 @@
 function Place() {
   return (
-    <div>Place</div>
+    <>
+      <article>
+        <section>
+          <h2>내 주변 보호소 찾기</h2>
+          <a href="">
+            <span>가까운 곳부터 천천히</span>
+            내 주변 보호소 찾기
+            </a>
+        </section>
+        <section>
+          <h2>추천 보호소</h2>
+          <div>
+            <img src="" alt="" />
+            <dl>
+              <dt>고양시 동물보호센터</dt>
+              <dd>주소: 경기도 고양시 덕양구 고양대로 1695</dd>
+            </dl>
+            <button>전화하기</button>
+            <button>위치보기</button>
+          </div>
+        </section>
+        <section>
+          <h2>더보기</h2>
+          <ul>
+            <li>
+              <img src="" alt="" />
+              <dl>
+                <dt>고양시 동물보호센터</dt>
+                <dd>주소: 경기도 고양시 덕양구 고양대로 1695</dd>
+                <dd>Tel : 042-825-1118</dd>
+              </dl>
+            </li>
+            <li>
+              <img src="" alt="" />
+              <dl>
+                <dt>고양시 동물보호센터</dt>
+                <dd>주소: 경기도 고양시 덕양구 고양대로 1695</dd>
+                <dd>Tel : 042-825-1118</dd>
+              </dl>
+            </li>
+          </ul>
+        </section>
+      </article>
+    </>
   )
 }
 
