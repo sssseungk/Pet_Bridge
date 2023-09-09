c6bf0f79844f445cc342ab484e842bcb84312b34
sssseungk <cacy0118@naver.com>
2023-09-09 14:46:57 +0900

    [Modify] 헤더 컴포넌트 아이콘 클릭시 이전 페이지로 이동 기능 추가
    #21 완료

--- src/components/Header/HeaderComp.jsx
+++ src/components/Header/HeaderComp.jsx
@@ -3,25 +3,38 @@ import logo_header_icon from '@/assets/icons/logo_header_icon.svg'
 import search_icon from '@/assets/icons/search_icon.svg'
 import cart_off_icon from '@/assets/icons/cart_off_icon.svg'
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 function HeaderComp({title='펫:브릿지', showLogo=false, showSearchIcon=false, showCartIcon=false}) {
+  
+  const navigate = useNavigate();
+  const handleGoPrevPage = () => {
+    navigate(-1);
+  }
+  
   return (
     <>
       <div className="max-w-4xl mx-auto p-5 bg-pet-bg border-gray-800 shadow-lg relative z-10">
         <div className="flex items-center justify-between">
-          <button>
+          <button onClick={handleGoPrevPage}>
             <img src={prev_icon}/>
           </button>
-          <h1 className="flex items-center gap-1 font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <h1 className="flex items-center gap-1 font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xl">
             {showLogo && <img src={logo_header_icon} className="w-5 h-5"/>}
             {title}
           </h1>
           <div>
             <button className="pr-4">
-              {showSearchIcon && <img src={search_icon}/>}
+              <Link to='/search'>
+                {showSearchIcon && <img src={search_icon}/>}
+              </Link>
             </button>
             <button>
-              {showCartIcon && <img src={cart_off_icon}/>}
+              <Link to='/cart'>
+                {showCartIcon && <img src={cart_off_icon}/>}
+              </Link>
             </button>
           </div>
         </div>
