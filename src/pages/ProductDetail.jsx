3d3bf2124c0028ac2ea4230cd449811c41aab26f
LKJ970524 <dlrbwjd7096@naver.com>
2023-09-10 22:45:44 +0900

    [Modify] ProductDetail 마크업 및 스타일링
    - ProductDetail 마크업진행중 입니다.
    
    #28 작업중

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,8 @@
 import useProductItem from "@/utils/useProductItem";
 import { useParams } from "react-router-dom";
 import { getPbImageURL } from "@/utils/getPbImageUrl";
+import minus from '/assets/icons/minus.svg'
+import plus from '/assets/icons/plus.svg'
 
 function ProductDetail() {
   const { productTitle } = useParams();
@@ -12,10 +14,23 @@ function ProductDetail() {
         <img src={getPbImageURL(data,'photo')} alt="상품사진" className=" m-auto h-64" />
       </div>
       <div className="text-xl pt-5">{data.title}</div>
-      <div className="text-xl pt-5">{data.price}</div>
-      <div className="">
-        <img src={getPbImageURL(data,'photo_detail')} alt="상품사진" className=" m-auto" />
+      <div className="flex justify-between">
+        <div className="text-xl pt-5">{data.price}</div>
+        <div className="count__btn flex h-[30px] w-[84px] flex-row flex-nowrap end items-center justify-start border-[1px] border-gray-2">
+          <button type="button" className="border-0 bg-transparent p-0 ">
+            <img src={minus} alt="수량 삭제" />
+          </button>
+          <span className="count mx-[8px] text-[16px] font-semibold text-black">
+            1
+          </span>
+          <button type="button" className="border-0 bg-transparent p-0">
+            <img src={plus} alt="수량 추가" />
+          </button>
+          </div>
       </div>
+      <div className='m-auto h-[1px] bg-black mt-4'></div>
+        <img src={getPbImageURL(data,'photo_detail')} alt="상품사진" className=" m-auto pt-4" />
+
 
     </div>
   )
