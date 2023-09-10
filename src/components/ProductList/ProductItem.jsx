8df865cee36e7168541ebb89f9e9803e55a769e0
sssseungk <cacy0118@naver.com>
2023-09-10 14:28:25 +0900

    [Modify] 상품 목록 페이지에 포켓호스트 상품 데이터 랜더링
    #31 진행중

--- src/components/ProductList/ProductItem.jsx
+++ src/components/ProductList/ProductItem.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
 import comment_icon from '/assets/icons/comment_icon.svg';
 import heart_empty_icon from '/assets/icons/heart_empty_icon.svg';
 import heart_fill_icon from '/assets/icons/heart_fill_icon.svg';
-import product1 from '/assets/imgs/product1.png';
 import { useState } from 'react';
+import {pb} from '@/api/pocketbase';
+import { getPbImageURL } from '@/utils/getPbImageUrl';
+import { PropTypes } from 'prop-types';
 
-function ProductItem() {
+const renderProductList = await pb.collection('product').getFullList();
+function ProductItem({product}) {
+
+  useEffect(() => {
+    try {
+      renderProductList;
+    } catch (error){
+      throw new Error('error');
+    }
+  }, []);
 
   const [addWish, setAddWish] = useState(false);
   const handleWishBtn = () => {
@@ -15,7 +27,7 @@ function ProductItem() {
     <div className='bg-[#FDF6EE] bg-pet-ivory rounded-[10px] w-full pet-360:w-[calc(50%/1-1rem)] pet-864:w-[calc(50%-1rem)] aspect-200/140'>
       <div className="flex-col items-center justify-center pt-3 px-3">
         <div className="relative">
-          <img src={product1} className="w-full h-3/2 rounded-[10px] transition-width duration-300"/>
+          <img src={getPbImageURL(product,'photo')} className="w-full h-3/2 rounded-[10px] transition-width duration-300"/>
           {
             addWish ? (
               <img src={heart_fill_icon} onClick={handleWishBtn} className="transition-all duration-300 hover:scale-125 cursor-pointer absolute pet-520:w-8 pet-520:top-4 pet-520:right-3 pet-864:w-12 pet-864:top-7 pet-864:right-6 top-[0.75rem] right-[0.75rem]"/>
@@ -24,8 +36,8 @@ function ProductItem() {
             )
           }
         </div>
-        <h1 className="transition-all duration-300 pet-520:text-base pet-864:text-2xl text-[12px] text-pet-black pt-2">베이비슈 리유저블 컵</h1>
-        <h2 className="transition-all duration-300 pet-520:text-sm pet-864:text-xl text-[10px] font-bold text-pet-red pt-1">5,900원</h2>
+        <h1 className="transition-all duration-300 pet-520:text-base pet-864:text-2xl text-[12px] text-pet-black pt-2">{product.title}</h1>
+        <h2 className="transition-all duration-300 pet-520:text-sm pet-864:text-xl text-[10px] font-bold text-pet-red pt-1">{product.price.toLocaleString('ko-KR')}원</h2>
         <div className="flex gap-1 justify-end pb-1 pt-2 pet-864:gap-2 pet-864:pr-3 pet-864:pb-3">
           <img src={comment_icon} className="transition-all duration-300 pet-520:w-4 pet-864:w-5"/>
           <span className="transition-all duration-300 text-gray-2 text-[8px] pet-520:text-sm pet-864:text-2xl">82</span>
@@ -36,3 +48,8 @@ function ProductItem() {
 }
 
 export default ProductItem
+
+
+ProductItem.propTypes = {
+  product: PropTypes.object,
+}
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,18 +1,32 @@
 import ProductItem from "@/components/ProductList/ProductItem"
 import ProductListNav from './../components/ProductList/ProductListNav';
+import {pb} from '@/api/pocketbase';
+import { useEffect } from "react";
+
+const renderProductList = await pb.collection('product').getFullList();
 
 function ProductList() {
+
+  useEffect(() => {
+    try{
+      renderProductList;
+      // console.log(renderProductList);
+    } catch(error){
+      throw new Error('error');
+    }
+  }, []);
   
   return (
-    <div>
+    <div className="bg-pet-bg">
       <ProductListNav/>
-      <ol className="flex flex-wrap max-w-4xl mx-auto justify-center mt-5 gap-2">
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
+      <ol className="bg-pet-bg flex flex-wrap max-w-4xl mx-auto justify-center mt-5 gap-2">
+        {renderProductList?.map(product => {
+          return (
+          <>   
+            <ProductItem product={product} key={product.id}/>
+          </>
+          )
+        })}
       </ol>
     </div>
   )
