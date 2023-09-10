adff7b78e16a51e39fb8366678c88f24569c6ca3
sssseungk <cacy0118@naver.com>
2023-09-10 16:22:03 +0900

    [Test] 상세페이지로 데이터 넘어가는지 테스트 하기 위한 상세페이지 코드 수정
    #34 진행중

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,28 @@
+import useProductItem from "@/utils/useProductItem";
+import { useParams } from "react-router-dom";
+import { getPbImageURL } from "@/utils/getPbImageUrl";
+
 function ProductDetail() {
+  const { productTitle } = useParams();
+  const { data } = useProductItem(productTitle);
+
   return (
-    <div>ProductDetail</div>
+    <div className="max-w-4xl m-auto bg-pet-bg pt-3 px-5">
+      <div className="rounded-2xl">
+        <img src={getPbImageURL(data,'photo')} alt="상품사진" className=" m-auto h-64" />
+      </div>
+      <div className="text-xl pt-5">{data.title}</div>
+      <div className="text-xl pt-5">{data.price}</div>
+      <div className="">
+        <img src={getPbImageURL(data,'photo_detail')} alt="상품사진" className=" m-auto" />
+      </div>
+
+    </div>
   )
 }
 
 export default ProductDetail
+
+
+
+
