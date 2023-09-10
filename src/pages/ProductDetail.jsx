743aa0278e4672a3e502ca5bfbc977866e292cf4
LKJ970524 <dlrbwjd7096@naver.com>
2023-09-10 16:04:18 +0900

    [Modify] ProductDetail 마크업 및 스타일링
    - ProductDetail 마크업진행중 커밋입니다.
    
    #28 작업중

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,6 @@
 import cup from '@/assets/imgs/cup_product.jpg';
-import minus from '@/assets/icons/minus.svg';
-import plus from '@/assets/icons/plus.svg';
+import minus from '/assets/icons/minus.svg';
+import plus from '/assets/icons/plus.svg';
 import detail1 from '@/assets/imgs/cupDetail_detail.jpg';
 
 function ProductDetail() {
@@ -8,7 +8,7 @@ function ProductDetail() {
     <>
       <div className="max-w-4xl m-auto bg-pet-bg pt-3 px-5">
         <div className="rounded-2xl">
-          <img src={cup} alt="메인" className=" m-auto h-64" />
+          <img src={cup} alt="상품사진" className=" m-auto h-64" />
         </div>
         <div className="text-xl pt-5">베이비슈 내열 리유저블컵</div>
         <div className="flex justify-between">
@@ -26,8 +26,13 @@ function ProductDetail() {
           </div>
         </div>
         <div className='m-auto h-[1px] bg-black mt-4'></div>
-        <img src={detail1} alt="메인" className=" m-auto pt-4" />
-
+        <img src={detail1} alt="상품상세정보" className=" m-auto pt-4" />
+        <div className='m-auto h-[1px] bg-black mt-4'></div>
+        <div className=' flex mt-2 m-auto border-[1px] rounded-3xl bg-[#FDF6EE] shadow-[2px_2px_4px_0_rgba(0,0,0,0.25)] pt-6 px-auto'>
+          <p className='justify-items-start'>이규정</p>
+          <p className='justify-items-end'>2023-09-10</p>
+          <p>제품이 너무이뻐욧</p>
+        </div>
       </div>
     </>
   );
