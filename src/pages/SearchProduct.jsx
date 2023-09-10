97fe0cbbe0f40b1ae0fec4ff5e94caa5940359f0
sssseungk <cacy0118@naver.com>
2023-09-10 16:23:58 +0900

    [Modify] 상품 검색 페이지 체크박스 테두리, 폰트 크기 수정
    #29 진행중

--- src/pages/SearchProduct.jsx
+++ src/pages/SearchProduct.jsx
@@ -16,16 +16,16 @@ function SearchProduct() {
       <div className="mt-2 max-w-4xl mx-auto pt-2 pl-3">
         <span className="text-sm font-[600]">친구들이 많이 찾고 있어요!</span>
         <ol className="flex gap-2 mt-3 justify-start">
-          <li className={`py-1 px-2 border-gray-1 border-2 rounded-[20px] ${isSelected === 'tumbler' ? 'bg-primary' : ''}`}>
+          <li className={`text-[12px] py-1 px-2 border-gray-1 border-[1px] rounded-[20px] ${isSelected === 'tumbler' ? 'bg-primary' : ''}`}>
             <button type="button" onClick={() => handleClickButton('tumbler')}>텀블러</button>
           </li>
-          <li className={`py-1 px-2 border-gray-1 border-2 rounded-[20px] ${isSelected === 'pouch' ? 'bg-primary' : ''}`}>
+          <li className={`text-[12px] py-1 px-2 border-gray-1 border-[1px] rounded-[20px] ${isSelected === 'pouch' ? 'bg-primary' : ''}`}>
             <button type="button" onClick={() => handleClickButton('pouch')}>파우치</button>
           </li>
-          <li className={`py-1 px-2 border-gray-1 border-2 rounded-[20px] ${isSelected === 'echobag' ? 'bg-primary' : ''}`}>
+          <li className={`text-[12px] py-1 px-2 border-gray-1 border-[1px] rounded-[20px] ${isSelected === 'echobag' ? 'bg-primary' : ''}`}>
             <button type="button" onClick={() => handleClickButton('echobag')}>에코백</button>
           </li>
-          <li className={`py-1 px-2 border-gray-1 border-2 rounded-[20px] ${isSelected === 'zibits' ? 'bg-primary' : ''}`}>
+          <li className={`text-[12px] py-1 px-2 border-gray-1 border-[1px] rounded-[20px] ${isSelected === 'zibits' ? 'bg-primary' : ''}`}>
             <button type="button" onClick={() => handleClickButton('zibits')}>지비츠</button>
           </li>
 
