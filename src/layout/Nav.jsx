d36c1bb7c709471fc68db1180507364cce393efd
LKJ970524 <dlrbwjd7096@naver.com>
2023-09-11 17:19:05 +0900

    [Modify] Nav z-index 적용
    - Nav가 밑으로 뭍히는 현상을 발견하여 z-index를 적용하였습니다.
    
    #10 작업중

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -7,7 +7,7 @@ import myPage from '/assets/icons/mypage2_icon.svg';
 function Nav() {
   return (
     <>
-      <nav className="max-w-4xl h-[52px] bg-pet-bg px-[28px] py-[16px] bottom-0 fixed rounded-b-xl left-0 right-0 m-auto border shadow-[0_-8px_20px_0_rgba(0,0,0,0.1)]">
+      <nav className="max-w-4xl h-[52px] z-10 bg-pet-bg px-[28px] py-[16px] bottom-0 fixed left-0 right-0 m-auto border shadow-[0_-8px_20px_0_rgba(0,0,0,0.1)]">
         <div className='flex justify-between items-center'>
           <button type="submit" className=''><img src={home} alt='메인' /></button>
           <button type="submit"><img src={shop} alt="쇼핑" /></button>
