671336e31ad3618460e86043d35636e9a17e0212
dongapple <hg971027@gmail.com>
2023-09-11 17:23:58 +0900

    [Modify] home 코드 추가 #24

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
+import HomeContact from '@/components/Home/HomeContact';
+import HomeCumulativeAmount from '@/components/Home/HomeCumulativeAmount';
+import HomePlace from '@/components/Home/HomePlace';
+import HomeShop from '@/components/Home/HomeShop';
+
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="max-w-screen-pet-l mx-auto ">
+      <HomeCumulativeAmount />
+      <HomeShop />
+      <HomeContact />
+      <HomePlace />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
