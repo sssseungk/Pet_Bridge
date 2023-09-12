8091ed3111efb4a68d71866da3b5f7f523d8a633
LKJ970524 <dlrbwjd7096@naver.com>
2023-09-12 17:30:37 +0900

    [Modify] nav 링크연결
    - nav바에서 다른 페이지로 이동하는 기능오류가 있어서 정상적으로 적용시켰습니다.
    
    #10 진행중

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -12,8 +12,8 @@ function Nav() {
       <nav className="max-w-4xl h-[52px] z-10 bg-pet-bg px-[28px] py-[16px] bottom-0 fixed left-0 right-0 m-auto border shadow-[0_-8px_20px_0_rgba(0,0,0,0.1)]">
         <div className='flex justify-between items-center'>
           <Link><button><img src={home} alt='메인' /></button></Link>
-          <Link to={`/ProductList`}><button><img src={shop} alt="쇼핑" /></button></Link>
-          <Link to={`/Place`}><button><img src={map} alt="장소" /></button></Link>
+          <Link to={`/productlist`}><button><img src={shop} alt="쇼핑" /></button></Link>
+          <Link to={`/place`}><button><img src={map} alt="장소" /></button></Link>
           <Link><button><img src={myPage} alt="마이페이지" /></button></Link>
         </div>
       </nav>
