139fb81262955da9f67cef9729b66aff2697df93
LKJ970524 <dlrbwjd7096@naver.com>
2023-09-12 15:35:39 +0900

    [Modify] ProductDetail 코드 수정
    - ProductDetail페이지에서 cart 링크가 제대로 안넘어가지는 오류를 발견하여 수정하였습니다
    
    #28 작업중

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -16,9 +16,7 @@ function ProductDetail() {
 
   return (
     <div className="max-w-4xl m-auto pt-3 px-5">
-      <div className="rounded-2xl">
-        <img src={getPbImageURL(data,'photo')} alt="상품사진" className=" m-auto h-64" />
-      </div>
+      <img src={getPbImageURL(data,'photo')} alt="상품사진" className=" m-auto h-64" />
       <div className="text-xl pt-5">{data.title}</div>
       <div className="flex justify-between">
         <div className="text-xl pt-5">{data.price} 원</div>
@@ -32,41 +30,39 @@ function ProductDetail() {
         
       </div>
       <div className='m-auto h-[1px] bg-black mt-4'></div>
-        <img src={getPbImageURL(data,'photo_detail')} alt="상품사진" className=" m-auto pt-4" />
-        <div className='m-auto h-[1px] bg-black mt-4 mb-2'></div>
-
-        <div className="bg-pet-bg h-auto flex-shrink-0 rounded-2xl shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25)] mb-6">
-          <div className="justify-center py-4 px-6">
-            <div className="flex justify-between pb-5 text-lg">
-              <p className="font-semibold">
-                조스마
-              </p>
-              <p className="font-semibold">2023.09.05</p>
-            </div>
-            <p className="text-xl">
+      <img src={getPbImageURL(data,'photo_detail')} alt="상품사진" className=" m-auto pt-4" />
+      <div className='m-auto h-[1px] bg-black mt-4 mb-2'></div>
+      <div className="bg-pet-bg h-auto flex-shrink-0 rounded-2xl shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25)] mb-6">
+        <div className="justify-center py-4 px-6">
+          <div className="flex justify-between pb-5 text-lg">
+            <p className="font-semibold">
+              조스마
+            </p>
+            <p className="font-semibold">2023.09.05</p>
+          </div>
+          <p className="text-xl">
             프로젝트하면서 계속 물마시는 일이 많아졌는데 동물그림이 그려져있어서 굉장히 귀엽고 또 동물을위한 후원도 할수있다는 점에서 구매했습니다 너무 좋아요!!!
-           </p>
-          </div>        
-        </div>
-
-        <div className="bg-pet-bg h-auto flex-shrink-0 rounded-2xl shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25)] mb-6">
-          <div className="justify-center py-4 px-6">
-            <div className="flex justify-between pb-5 text-lg">
-              <p className="font-semibold">
-                김서기
-              </p>
-              <p className="font-semibold">2023.09.05</p>
-            </div>
-            <p className="text-xl">
+          </p>
+        </div>        
+      </div>
+      <div className="bg-pet-bg h-auto flex-shrink-0 rounded-2xl shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25)] mb-6">
+        <div className="justify-center py-4 px-6">
+          <div className="flex justify-between pb-5 text-lg">
+            <p className="font-semibold">
+              김서기
+            </p>
+            <p className="font-semibold">2023.09.05</p>
+          </div>
+          <p className="text-xl">
             텀블러가 너무 귀엽고 후원도 된다고 해서 한 번 사봤는데 너무 유용하게 잘 쓰고 있어요! 너무 좋은 앱인것 같아요 ^^!
-           </p>
-          </div>        
-        </div>
-        <Link to={`/Cart`} >
-          <button className="w-full m-auto h-12 bg-pet-orange rounded-lg items-center mb-3 text-sm bottom-16 left-0 right-0 sticky">
-            장바구니
-          </button>
-        </Link>
+          </p>
+        </div>        
+      </div>
+      <Link to={`/cart`} >
+        <button className="w-full m-auto h-12 bg-primary rounded-lg items-center mb-3 text-base bottom-16 left-0 right-0 sticky">
+          장바구니
+        </button>
+      </Link>
     </div>
   )
 }
