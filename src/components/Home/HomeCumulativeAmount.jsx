32059aedb0a41b3d48a346833e6e717131d101e8
dongapple <hg971027@gmail.com>
2023-09-13 23:12:43 +0900

    [Modify] 홈 화면 로그인 회원가입 버튼 추가 #24

--- src/components/Home/HomeCumulativeAmount.jsx
+++ src/components/Home/HomeCumulativeAmount.jsx
@@ -1,16 +1,34 @@
 import sample from '/assets/imgs/Home_dog.png';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function HomeCumulativeAmount(props) {
   return (
-    <section className="bg-primary relative py-7 px-5">
+    <section className="relative px-5 py-10 bg-primary">
       <h2 className="font-bold">여러분들의 후원으로 이만큼이나 모였어요.</h2>
       <span className="text-sm font-semibold">누적후원금액</span>
       <p className="text-2xl font-black">
         <span>{props.Amount.toLocaleString('ko-KR')}</span>원
       </p>
+      <ul className="absolute flex gap-2 right-5 top-10 hide-on-mobile">
+        <li>
+          <Link to={`/signin`}>로그인</Link>
+        </li>
+        <li>
+          <Link to={`/signup`}>회원가입</Link>
+        </li>
+      </ul>
+      <style>
+        {`
+          @media (max-width: 576px) {
+            .hide-on-mobile {
+              display: none;
+            }
+          }
+        `}
+      </style>
       <div
-        className="bg-white w-full h-5 rounded-t-full absolute bottom-0 left-0"
+        className="absolute bottom-0 left-0 w-full h-5 bg-white rounded-t-full "
         style={{ boxShadow: '0px -12px 12px rgba(0, 0, 0, 0.08)' }}
       >
         <img
