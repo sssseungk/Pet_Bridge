34acfad76f01f9e34d4ca34ee4c1608862010773
tuna7797 <gmlwjd7731@gmail.com>
2023-09-13 17:10:30 +0900

    [Add] 회원가입 페이지 제작
    - SignUp.jsx 추가
    - 디바운스 적용
    - routes 경로 추가
    
    #55 진행중

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,16 +6,20 @@ import {
 import RootLayout from "./layout/RootLayout";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import Map from "./pages/Map";
 import Place from "./pages/Place";
 import ProductDetail from "./pages/ProductDetail";
 import ProductList from "./pages/ProductList";
 import SearchProduct from "./pages/SearchProduct";
-import Map from "./pages/Map";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
+      <Route path="signin" element={<SignIn />} />
+      <Route path="signup" element={<SignUp />} />
       <Route path="cart" element={<Cart/>} />
       <Route path="place" element={<Place/>} />
       <Route path="productlist" element={<ProductList />} />
