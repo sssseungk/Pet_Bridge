6bfe95a90d24eabdefe9f7b040d68cf333501589
tuna7797 <gmlwjd7731@gmail.com>
2023-09-16 20:47:18 +0900

    [Setting] 배포를 위한 코드 수정 - vite.config base 경로 추가 - vite.config esbuild 추가 - routes.jsx 경로 추가 및 Hash로 변경
    #86 진행중

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,28 @@
-import react from "@vitejs/plugin-react";
-import { resolve } from "node:path";
-import { defineConfig } from "vite";
-import { env } from "node:process";
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import { env } from 'node:process';
 
-const idDev = env.NODE_ENV === "development";
+const idDev = env.NODE_ENV === 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Pet_Bridge/',
   plugins: [react()],
   css: {
     devSourcemap: true,
     modules: {
       generateScopedName: idDev
-        ? "[name]_[local]__[hash:base64:5]"
-        : "[hash:base64:4]",
+        ? '[name]_[local]__[hash:base64:5]'
+        : '[hash:base64:4]',
     },
+  },
+  esbuild: {
+    target: 'es2020',
   },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
   },
 });
