e7c264bdcec5e405dc90450092d1662a62765390
sssseungk <cacy0118@naver.com>
2023-09-17 01:18:42 +0900

    [Modify] 상품 목록 페이지로 이동될 때 스피너 추가
    #23 완료

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,19 +1,28 @@
 import ProductItem from "@/components/ProductList/ProductItem"
 import ProductListNav from './../components/ProductList/ProductListNav';
-import {pb} from '@/api/pocketbase';
+import pb from '@/api/pocketbase';
 import { useEffect } from "react";
 import { useState } from "react";
+import Spinner from "@/components/Common/Spinner";
+import { useRef } from "react";
 
 
 function ProductList() {
+  const [isLoading, setIsLoading] = useState(false);
   const [productList, setProductList] = useState([]);
   const [reviewCounts, setReviewCounts] = useState({});
   const [selectedCategory, setSelectedCategory] = useState('전체');
+  const isFirstMountRef = useRef(true);
   let displayProducts = productList;
 
   useEffect(() => {
+    if(isFirstMountRef.current) {
+      isFirstMountRef.current = false;
+      return;
+    }
     const getProductList = async () => {
       try {
+        setIsLoading(true);
         const productListData = await pb.collection('product').getFullList();
         setProductList(productListData);
         const reviewsData = await pb.collection('reviews').getFullList();
@@ -24,11 +33,20 @@ function ProductList() {
         setReviewCounts(counts);
       } catch (error) {
         throw new Error('Error fetching product list');
+      } finally {
+        setIsLoading(false);
       }
     };
     getProductList();
   }, []);
   
+  if((isFirstMountRef.current && !productList.length) || isLoading){
+    return (
+      <div className="grid place-content-center h-full">
+        <Spinner size={120} className="mt-8"/>
+      </div>
+    );
+  }
 
   if (selectedCategory === '무료배송') {
     displayProducts = productList.filter(product => product.delivery_free);
