2d3853a1cd5c3bdc255f109ed152dab9b99a1957
dongapple <hg971027@gmail.com>
2023-09-19 13:37:59 +0900

    [Modify] 어바웃 베너 수정 #24

--- src/components/Home/GoAbout.jsx
+++ src/components/Home/GoAbout.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
 import ppl_hand from '/assets/icons/ppl_hand_icon.svg';
 import cat_hand from '/assets/icons/cat_hand_icon.svg';
 
@@ -7,16 +9,26 @@ function GoAbout(props) {
     <section className="flex flex-col gap-3 px-5 py-3">
       <div className="flex items-center justify-between">
         <h2 className="inline font-bold">{props.title}</h2>
-        <span className="text-xs">더보기 &gt;</span>
+        <Link
+          to={`/about`}
+          className="text-xs"
+          onClick={() => window.scrollTo(0, 0)}
+        >
+          더보기 &gt;
+        </Link>
       </div>
-      <div className="relative flex flex-row items-center justify-between gap-3 py-3 rounded-lg bg-pet-green">
+      <Link
+        to={`/about`}
+        onClick={() => window.scrollTo(0, 0)}
+        className="relative flex flex-row items-center justify-between gap-3 py-3 rounded-lg bg-pet-green"
+      >
         <img src={ppl_hand} alt="" className="w-[20%] max-w-[80px]" />
         <span className="text-2xl font-bold text-center text-pet-bg">
           About Us <br />
           펫:브릿지
         </span>
         <img src={cat_hand} alt="" className="w-[20%] max-w-[80px]" />
-      </div>
+      </Link>
     </section>
   );
 }
