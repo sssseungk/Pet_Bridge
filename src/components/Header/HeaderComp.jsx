0086f66d3ba21434e76c0204ead46cfc5b634d11
sssseungk <cacy0118@naver.com>
2023-09-19 11:31:52 +0900

    [Modify] 헤더 장바구니에 상품 있을 때 아이콘 변경, 키보드 접근성 수정
    #112 완료

--- src/components/Header/HeaderComp.jsx
+++ src/components/Header/HeaderComp.jsx
@@ -1,10 +1,12 @@
 import prev_icon from '/assets/icons/prev_icon.svg';
 import logo_header_icon from '/assets/icons/logo_header_icon.svg';
 import search_icon from '/assets/icons/search_icon.svg';
+import cart_on_icon from '/assets/icons/cart_on_icon.svg';
 import cart_off_icon from '/assets/icons/cart_off_icon.svg';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { useAuth } from '@/contexts/Auth';
 
 function HeaderComp({
   title,
@@ -13,6 +15,10 @@ function HeaderComp({
   showCartIcon,
   showPrevIcon,
 }) {
+  const user = useAuth();
+  const isCartFilled =
+    user && user.user && user.user.AddCart && user.user.AddCart.length > 0;
+
   const navigate = useNavigate();
   const handleGoPrevPage = () => {
     switch (location.pathname) {
@@ -38,27 +44,30 @@ function HeaderComp({
       <div className="max-w-screen-pet-l mx-auto p-5 bg-pet-bg border-gray-800 shadow-lg relative z-10">
         <div className="flex items-center justify-between">
           <button onClick={handleGoPrevPage}>
-            {showPrevIcon && <img src={prev_icon} />}
+            {showPrevIcon && <img src={prev_icon} alt="뒤로가기" />}
           </button>
           <h1 className="flex items-center gap-1 font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xl">
             {showLogo && (
-              <Link to="/home">
-                <img src={logo_header_icon} className="w-5 h-5" />
-              </Link>
+              <img
+                src={logo_header_icon}
+                alt="펫브릿지 로고"
+                className="w-5 h-5"
+              />
             )}
             {title}
           </h1>
-          <div>
-            <button className="pr-4">
-              <Link to="search">
-                {showSearchIcon && <img src={search_icon} />}
-              </Link>
-            </button>
-            <button>
-              <Link to="cart">
-                {showCartIcon && <img src={cart_off_icon} />}
-              </Link>
-            </button>
+          <div className="flex gap-4">
+            <Link to="search">
+              {showSearchIcon && <img src={search_icon} alt="검색" />}
+            </Link>
+            <Link to="cart">
+              {showCartIcon && (
+                <img
+                  src={isCartFilled ? cart_on_icon : cart_off_icon}
+                  alt="장바구니"
+                />
+              )}
+            </Link>
           </div>
         </div>
       </div>
