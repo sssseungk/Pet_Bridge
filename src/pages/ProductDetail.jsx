06e6bbdb1385fff21608dc9ef5a7c9c8497efdd4
LKJ970524 <dlrbwjd7096@naver.com>
2023-09-20 16:55:07 +0900

    [Modify] detail페이지
    - detail페이지 헤더 코드 추가
    
    #88 완료

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -21,11 +21,8 @@ function ProductDetail() {
   const [lastReviewId, setLastReviewId] = useState(null);
   const [editingCommentId, setEditingCommentId] = useState(null);
   const [editingContent, setEditingContent] = useState('');
-  const [activeSection, setActiveSection] = useState('');
 
   useEffect(() => {
-    
-
     // * 리뷰 연결
     const fetchReviews = async () => {
       try {
@@ -174,21 +171,21 @@ function ProductDetail() {
     }
   };
 
-    // * 상품 수량 관리
-    const [quantity, setQuantity] = useState(1);
+  // * 상품 수량 관리
+  const [quantity, setQuantity] = useState(1);
 
-    // * 수량 증가 함수
-    const increaseCount = () => {
-      setQuantity(quantity + 1);
-    };
-  
-    // * 수량 감소 함수
-    const decreaseCount = () => {
-      if (quantity > 1) {
-        // 최소 1개 이상이어야 함.
-        setQuantity(quantity - 1);
-      }
-    };
+  // * 수량 증가 함수
+  const increaseCount = () => {
+    setQuantity(quantity + 1);
+  };
+
+  // * 수량 감소 함수
+  const decreaseCount = () => {
+    if (quantity > 1) {
+      // 최소 1개 이상이어야 함.
+      setQuantity(quantity - 1);
+    }
+  };
 
   // * 장바구니 담기
 
@@ -225,12 +222,15 @@ function ProductDetail() {
       return;
     }
     try {
-    // 현재 사용자의 모든 장바구니 아이템 가져오기
-    const userCartItems = await pb.collection('userCart').getFullList(`userId="${user.id}"`);
+      // 현재 사용자의 모든 장바구니 아이템 가져오기
+      const userCartItems = await pb
+        .collection('userCart')
+        .getFullList(`userName="${user.name}"`);
 
-    // 선택한 상품이 이미 있는지 확인하기 (현재 사용자에 한함)
-    const existingCartItem = userCartItems.find(item => item.productId === data.id && item.userId === user.id);
-  
+      // 선택한 상품이 이미 있는지 확인하기
+      const existingCartItem = userCartItems.find(
+        (item) => item.productId === data.id
+      );
       // 만약 이미 존재한다면, 토스트 메시지 띄우고 함수 종료
       if (existingCartItem) {
         toast('이미 추가된 상품입니다.', {
@@ -243,33 +243,46 @@ function ProductDetail() {
         });
         return;
       }
-      
+
       const newCartData = await pb.collection('userCart').create({
         userId: user.id,
         userName: user.name,
         productId: data.id,
         quantity: quantity,
-       });
-  
-       console.log(newCartData);
-  
-       const expandedCartData = await pb
-         .collection('userCart')
-         .getFullList(`userName="${user.name}"`);
-       console.log(expandedCartData);
-       toast('상품이 추가되었습니다.', {
-         position:'top-center', 
-         icon:'🛒', 
-         ariaProps:{
-           role:'alert', 
-           'aria-live':'polite'
-         }
-       });
-     } catch (error) { 
-       console.log(error); 
-     }
+      });
+
+      console.log(newCartData);
+
+      const expandedCartData = await pb
+        .collection('userCart')
+        .getFullList(`userName="${user.name}"`);
+      console.log(expandedCartData);
+
+      // ! 코드 새로 추가 (헤더 아이콘용)------------------------
+      const cartData = await pb
+        .collection('userCart')
+        .getFullList(`userName="${user.name}"`);
+      const relatedCarts = cartData.filter(
+        (item) => item.userName === user.name
+      );
+      let userRelatedCarts = relatedCarts.map((item) => item.id);
+      await pb
+        .collection('users')
+        .update(user.id, { userCart: userRelatedCarts });
+      // ! 여기 까지-----------------------------------
+      toast('상품이 추가되었습니다.', {
+        position: 'top-center',
+        icon: '🛒',
+        ariaProps: {
+          role: 'alert',
+          'aria-live': 'polite',
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
-  
+
   const scrollToElement = (elementId) => {
     const element = document.getElementById(elementId);
     window.scrollTo({
@@ -278,6 +291,8 @@ function ProductDetail() {
     });
   };
 
+  // 상태변경 함수 선언
+  const [activeSection, setActiveSection] = useState('');
   // 스크롤 포인트 지정
   const checkScrollPosition = () => {
     const productDescription =
@@ -285,8 +300,8 @@ function ProductDetail() {
     const productDetails = document.getElementById('productDetails').offsetTop;
     const reviews = document.getElementById('reviews').offsetTop;
 
-// 스크롤 포인트에 따라 상태 바꾸고
-if (window.pageYOffset >= reviews) {
+    // 스크롤 포인트에 따라 상태 바꾸고
+    if (window.pageYOffset >= reviews) {
       setActiveSection('reviews');
     } else if (window.pageYOffset >= productDetails) {
       setActiveSection('productDetails');
@@ -294,7 +309,8 @@ if (window.pageYOffset >= reviews) {
       setActiveSection('productDescription');
     }
   };
-useEffect(() => {
+  // useEffect 사용
+  useEffect(() => {
     window.addEventListener('scroll', checkScrollPosition);
 
     return () => window.removeEventListener('scroll', checkScrollPosition);
@@ -302,7 +318,12 @@ useEffect(() => {
 
   return (
     <div className="max-w-screen-pet-l m-auto pt-3 px-5">
-      <img id='productDescription' src={getPbImageURL(data, 'photo')} alt="상품사진" className="m-auto h-auto"/>
+      <img
+        id="productDescription"
+        src={getPbImageURL(data, 'photo')}
+        alt="상품사진"
+        className="m-auto h-auto"
+      />
       <div className="flex justify-between">
         <div className="text-xl pt-5">{data.title}</div>
         <div className="flex mt-5 mx-3">
@@ -328,33 +349,62 @@ useEffect(() => {
         ) : (
           <div className="text-xl pt-5">가격 정보 없음</div>
         )}
-        <button onClick={handleAddCart} className="bg-primary w-32 h-9 rounded-xl mt-3">
+        <button
+          onClick={handleAddCart}
+          className="bg-primary w-32 h-9 rounded-xl mt-3"
+        >
           장바구니 추가
         </button>
       </div>
-      <h2 className='top-0 sticky'>
-        <ul className="max-w-4xl h-14 bg-pet-bg font-bold flex justify-evenly border-pet-black border-2">
-          <li onClick={() => scrollToElement('productDescription')}
-          className={`py-3 border-r-2 border-pet-black text-center w-[33.3%] cursor-pointer hover:text-pet-green ${activeSection === 'productDescription' ? 'bg-primary' : '' }`}>
-            상품사진
-          </li>
-          <li onClick={() => scrollToElement('productDetails')} 
-          className={`py-3 border-r-2 border-pet-black text-center w-[33.3%] cursor-pointer hover:text-pet-green ${activeSection === 'productDetails' ? 'bg-primary' : '' }`}>
-            상세정보
-          </li>
-          <li onClick={() => scrollToElement('reviews')} 
-          className={`py-3 text-center w-[33.3%] cursor-pointer hover:text-pet-green ${activeSection === 'reviews' ? 'bg-primary' : '' }`}>
-            리뷰
-          </li>
-        </ul>
-      </h2>
-      <h2>
-        <img id='productDetails' src={getPbImageURL(data, 'photo_detail')} alt="상품사진" className="m-auto py-4 border-b-2 border-pet-black "/>
-      </h2>
-      <h2 className='text-2xl my-3 mx-4 bg-pet-bg'>Review</h2>
-      <form id='reviews' className="py-4 mx-4 flex" onSubmit={editingCommentId ? handleEditSubmit : handleCommentSubmit}>
-        <textarea type="text" value={comment} onChange={(e) => setComment(e.target.value)} placeholder="작성하실 리뷰를 적어주세요" className="border w-60 h-9"/>
-        <button type="submit" onClick={handleCommentSubmit} className="border ml-5 bg-primary w-14 h-9 rounded-xl">
+      <ul className="max-w-4xl h-14 bg-pet-bg font-bold flex justify-evenly border top-0 sticky">
+        <li
+          onClick={() => scrollToElement('productDescription')}
+          className={`py-3 border-r text-center w-[33.3%] cursor-pointer hover:text-pet-green ${
+            activeSection === 'productDescription' ? 'bg-primary' : ''
+          }`}
+        >
+          상품사진
+        </li>
+        <li
+          onClick={() => scrollToElement('productDetails')}
+          className={`py-3 border-r text-center w-[33.3%] cursor-pointer hover:text-pet-green ${
+            activeSection === 'productDetails' ? 'bg-primary' : ''
+          }`}
+        >
+          상세정보
+        </li>
+        <li
+          onClick={() => scrollToElement('reviews')}
+          className={`py-3 text-center w-[33.3%] cursor-pointer hover:text-pet-green ${
+            activeSection === 'reviews' ? 'bg-primary' : ''
+          }`}
+        >
+          리뷰
+        </li>
+      </ul>
+      <img
+        id="productDetails"
+        src={getPbImageURL(data, 'photo_detail')}
+        alt="상품사진"
+        className="m-auto py-4 border-b"
+      />
+      <form
+        id="reviews"
+        className="py-4 mx-4 flex"
+        onSubmit={editingCommentId ? handleEditSubmit : handleCommentSubmit}
+      >
+        <textarea
+          type="text"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          placeholder="작성하실 리뷰를 적어주세요"
+          className="border w-60 h-9"
+        />
+        <button
+          type="submit"
+          onClick={handleCommentSubmit}
+          className="border ml-5 bg-primary w-14 h-9 rounded-xl"
+        >
           작성
         </button>
       </form>
@@ -372,21 +422,42 @@ useEffect(() => {
             .replace(/\. /g, '.');
           const commentUser = review.expand && review.expand.users;
           return (
-            <div key={index} className="bg-pet-bg h-auto flex-shrink-0 rounded-2xl shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25)] mb-6">
+            <div
+              key={index}
+              className="bg-pet-bg h-auto flex-shrink-0 rounded-2xl shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25)] mb-6"
+            >
               <div className="py-4 px-6">
                 <div className="flex text-2xl items-center">
-                {commentUser && commentUser.avatar ? (
-                  <img src={getPbImageURL(commentUser, 'avatar')} alt={commentUser.name} className="w-11 h-11 rounded-lg"/>
+                  {commentUser && commentUser.avatar ? (
+                    <img
+                      src={getPbImageURL(commentUser, 'avatar')}
+                      alt={commentUser.name}
+                      className="w-11 h-11 rounded-lg"
+                    />
                   ) : (
-                  <img src={profileImg_default} alt="프로필 이미지" className="w-11 h-11 rounded-lg"/>
+                    <img
+                      src={profileImg_default}
+                      alt="프로필 이미지"
+                      className="w-11 h-11 rounded-lg"
+                    />
                   )}
                   <p className="pl-2 font-semibold">{review.name}</p>
                 </div>
                 <p className="text-sm font-semibold mb-5">{formattedDate}</p>
                 {editingCommentId === review.id ? (
                   <>
-                    <textarea type="text" value={editingContent} onChange={handleEditChange} className="w-full"/>
-                    <button className="bg-primary w-14 h-9 rounded-xl" onClick={handleEditSubmit}>완료</button>
+                    <textarea
+                      type="text"
+                      value={editingContent}
+                      onChange={handleEditChange}
+                      className="w-full"
+                    />
+                    <button
+                      className="bg-primary w-14 h-9 rounded-xl"
+                      onClick={handleEditSubmit}
+                    >
+                      완료
+                    </button>
                   </>
                 ) : (
                   <div>
@@ -394,10 +465,16 @@ useEffect(() => {
                     <div className="flex justify-end mt-3 gap-3">
                       {user?.name === review?.name && (
                         <>
-                          <button className="bg-primary w-14 h-9 rounded-xl" onClick={() => handleCommentEdit(review.id)}>
+                          <button
+                            className="bg-primary w-14 h-9 rounded-xl"
+                            onClick={() => handleCommentEdit(review.id)}
+                          >
                             수정
                           </button>
-                          <button className="bg-primary w-14 h-9 rounded-xl" onClick={() => handleCommentDelete(review.id)}>
+                          <button
+                            className="bg-primary w-14 h-9 rounded-xl"
+                            onClick={() => handleCommentDelete(review.id)}
+                          >
                             삭제
                           </button>
                         </>
@@ -409,12 +486,12 @@ useEffect(() => {
             </div>
           );
         })}
-        <Link to={`/cart`}>
-          <button className="w-full m-auto h-12 bg-primary rounded-lg items-center mb-3 text-base bottom-16 left-0 right-0 sticky">
-            장바구니
-          </button>
-        </Link>
-      </div>
+      <Link to={`/cart`}>
+        <button className="w-full m-auto h-12 bg-primary rounded-lg items-center mb-3 text-base bottom-16 left-0 right-0 sticky">
+          장바구니
+        </button>
+      </Link>
+    </div>
   );
 }
 
